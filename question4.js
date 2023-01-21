8ab54d7aacd3831d0c5cecf0356c7588de6fb673
ahsn.dev <ahsn.dev@gmail.com>
2023-01-21 18:48:27 +0330

    create question4.js file and add showNotification function

---
+++ question4.js
@@ -0,0 +1,14 @@
+// function showNotification(obj) {
+//   const top = prompt("enter your top position: ");
+//   const right = prompt("enter your right position: ");
+//   const message = prompt("enter your message: ");
+//   obj.top = top;
+//   obj.right = right;
+//   obj.message = message;
+// }
+
+// showNotification({
+//   top: "10px",
+//   right: "10px",
+//   message: "Hello World",
+// });
